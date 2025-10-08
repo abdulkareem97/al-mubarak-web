e89d448bf14f395858e8b63961a2f515c1e0428d
Abdul Kareem <rajab8321@gmail.com>
2025-10-08 18:49:10 +0530

    fix: issue fixed of home page

--- app/[dashboard]/page.tsx
+++ app/[dashboard]/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import { useAuth } from "@/hooks/useAuth";
+import React, { useEffect } from "react";
 
 const Page = () => {
-  return <div></div>;
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      window.location.href = "/login";
+    } else {
+      window.location.href = "/dashboard/overview";
+    }
+  }, [user]);
+  return <div>Home Page</div>;
 };
 
 export default Page;
